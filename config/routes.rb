600fd6fc2aa2578611506685a554a85778676069
ln1draw <shanghaiellen@gmail.com>
2018-01-08 22:16:44 -0800

    quick contact/about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'about', to: 'welcome#about'
+  get 'contact', to: 'welcome#contact'
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
