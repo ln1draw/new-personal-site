46f24217e2b5e3b6661fe5fb2ec29c5622cf612b
ln1draw <shanghaiellen@gmail.com>
2018-01-09 21:41:47 -0800

    layout tweaks

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,13 @@
 class WelcomeController < ApplicationController
   def index
+    @title = "Ellen Wondra"
   end
 
   def about
+    @title = "About Ellen Wondra"
   end
 
   def contact
+    @title = "Contact Ellen Wondra"
   end
 end
