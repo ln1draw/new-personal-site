ce058e9c4f58fd8a91339bb6a04d04652ed3996f
ellen wondra <ellenfromillinois@gmail.com>
2018-03-13 20:00:16 -0700

    updates routes to redirect bad urls and updates tabs in controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,13 @@
 class WelcomeController < ApplicationController
-  def index
-    @title = "Ellen Wondra"
-  end
+    def index
+        @title = "Ellen Wondra"
+    end
 
-  def resume
-    @title = "Resume | Ellen Wondra"
-  end
+    def resume
+        @title = "Resume | Ellen Wondra"
+    end
 
-  def links
-    @title = "Links | Ellen Wondra"
-  end
+    def links
+        @title = "Links | Ellen Wondra"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'links', to: 'welcome#links'
 
   root 'welcome#index'
+  match "*path", to: redirect('/'), via: :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
