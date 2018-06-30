f1cb76f1dbc00198bbb16fd0f39254207d8b7c72
ellen wondra <ellenfromillinois@gmail.com>
2018-06-30 12:53:49 -0700

    fixes bug in counter

--- app/controllers/counter_controller.rb
+++ app/controllers/counter_controller.rb
@@ -3,14 +3,15 @@ class CounterController < ApplicationController
   end
 
   def add_one
-    @c = Counter.first
+    @c = Counter.first_or_create
+    @c.clicks = 0 if @c.clicks.nil?
     @c.clicks = @c.clicks + 1
     @c.save!
     render json: { clicks: @c.clicks }
   end
 
   def show
-    @c = Counter.first
+    @c = Counter.first_or_create
     render json: { clicks: @c.clicks }
   end
 end
