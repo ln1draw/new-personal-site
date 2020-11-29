ca335c112285d4c1cfc450cc067738467c8323e6
ellen wondra <ellenfromillinois@gmail.com>
2020-11-28 18:51:10 -0800

    initial rebrand work

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 Rails.application.routes.draw do
   get 'counter/index'
-
   get 'counter/add_one'
-
   get 'counter/show'
 
   get 'welcome/index'
 
-  get 'resume',          to: 'welcome#resume'
-  get 'links',           to: 'welcome#links'
-  get 'music',           to: 'welcome#music'
-  get 'qr-cross-stitch', to: 'welcome#qrstitch'
+  get 'qr-cross-stitch', to: redirect('/')
   get 'stitchify',       to: 'stitchify#index'
-  get 'stitchify/svg',   to: 'stitchify#svg'
-
-  mount Monologue::Engine, at: '/blog'
 
   root 'welcome#index'
   match "*path", to: redirect('/'), via: :all
